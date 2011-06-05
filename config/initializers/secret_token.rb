7afa703c04d3a85f9ef626f76d2bd12e13384f03
Boerje <boerje.santen@freenet.de>
2011-06-05 10:27:28 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pixelpoint::Application.config.secret_token = '260ae05aebc1d7d56cc217019ee96b507d515bb73ad7d5ec0a2699e8cbb8747bffaa2a78a037e21f82c697bf461c085cacc59dfa0a110602864da9ce85515895'
